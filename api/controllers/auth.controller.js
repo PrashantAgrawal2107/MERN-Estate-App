c7626faf83b708004911a75f34cc9f6cf357cdeb
Prashant Agrawal <prashantagrawal2107@gmail.com>
2024-07-28 15:29:30 +0530

    create a middleware and a function to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'; 
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req,res) => {
+export const signup = async (req,res,next) => {
     // console.log(req.body);
     const {username , email , password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password , 10);
@@ -10,7 +11,9 @@ export const signup = async (req,res) => {
         await newUser.save();
         res.status(201).json("User created successfully !");
     }catch(error){
-        res.status(500).json(error.message);
+        // next(errorHandler(550 , 'Error from the Database'));
+        next(error);
+        // res.status(500).json(error.message);
     }
 }
     
--- api/index.js
+++ api/index.js
@@ -22,6 +22,16 @@ app.listen(3000 , ()=>{
 app.use('/api/user' , userRouter);
 app.use('/api/auth' , authRouter);
 
+app.use((err , req , res , next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message
+    })
+});
+
 // app.get('/' , (req,res)=>{
 //     // res.send('Hello World');
 //     res.json({
